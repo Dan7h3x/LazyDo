e823f1c0804f73246a76d0034cc5369bd2f9323b
Dan7h3x <m.jalili.barbin@gmail.com>
2024-12-17 14:17:46 +0330

    add

--- lua/lazydo/init.lua
+++ lua/lazydo/init.lua
@@ -2,10 +2,36 @@
 local LazyDo = require('lazydo.core')
 local setup = require('lazydo.setup')
 
--- Re-export the module
-return setmetatable({}, {
-  __index = LazyDo,
-  __call = function(_, opts)
-    return setup(opts)
+-- Create the setup function that lazy.nvim will call
+local M = {
+  _instance = nil
+}
+
+function M.setup(opts)
+  if not M._instance then
+    M._instance = setup(opts)
   end
-})
+  return M._instance
+end
+
+-- Add convenience methods
+function M.toggle()
+  if M._instance then
+    M._instance:toggle()
+  end
+end
+
+function M.show()
+  if M._instance then
+    M._instance:show()
+  end
+end
+
+function M.close()
+  if M._instance then
+    M._instance:close_window()
+  end
+end
+
+-- Re-export the module
+return M
